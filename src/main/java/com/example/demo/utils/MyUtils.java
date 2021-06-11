a5db9dbaa39807c8193680a4da9113e51d1e19bd
iswarning <59012163+iswarning@users.noreply.github.com>
2021-06-11 13:11:14 +0700

    Create MyUtils.java

---
+++ src/main/java/com/example/demo/utils/MyUtils.java
@@ -0,0 +1,20 @@
+public class MyUtils
+{
+  // check if string contain number
+  public static Integer stringContainDigit(final String str) {                
+    
+    if(StringUtils.hasText())
+    {
+      StringBuilder sb = new StringBuilder();
+      for(char c : str.toCharArray()){
+          if(Character.isDigit(c)){
+              return 1;
+          } else {
+              return 0;                
+          }
+      }
+    }
+    
+    return -1;
+}
+}
